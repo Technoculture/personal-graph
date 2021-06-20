48450df63447efb1138aaa063a80cdd185336ffa
Denis Papathanasiou <denis@papathanasiou.org>
2021-06-20 10:45:04 -0400

    Corresponding test and fixture updates

--- tests/database_test.py
+++ tests/database_test.py
@@ -122,6 +122,20 @@ def test_traversal(database_test_file, apple):
         '5', '1', '2', '3', '4']
 
 
+def test_traversal_with_bodies(database_test_file, apple):
+    def _normalize_results(results):
+        return [(x, y, json.loads(z)) for (x, y, z) in results]
+
+    assert _normalize_results(db.traverse_with_bodies(database_test_file, 2, 3)) == _normalize_results([('2', '()', '{"name":"Steve Wozniak","type":["person","engineer","founder"],"id":2}'), ('1', '->', '{"action":"founded"}'), ('3', '->', '{}'), ('1', '()', '{"name":"Apple Computer Company","type":["company","start-up"],"founded":"April 1, 1976","id":1}'), (
+        '2', '<-', '{"action":"founded"}'), ('3', '<-', '{"action":"founded"}'), ('4', '<-', '{"action":"founded"}'), ('5', '<-', '{"action":"invested","equity":80000,"debt":170000}'), ('4', '->', '{"action":"divested","amount":800,"date":"April 12, 1976"}'), ('3', '()', '{"name":"Steve Jobs","type":["person","designer","founder"],"id":3}')])
+    assert _normalize_results(db.traverse_with_bodies(database_test_file, 5, neighbors_fn=db.find_inbound_neighbors)) == _normalize_results(
+        [('5', '()', '{"name":"Mike Markkula","type":["person","investor"],"id":5}')])
+    assert _normalize_results(db.traverse_with_bodies(database_test_file, 5, neighbors_fn=db.find_outbound_neighbors)) == _normalize_results([('5', '()', '{"name":"Mike Markkula","type":["person","investor"],"id":5}'), ('1', '->', '{"action":"invested","equity":80000,"debt":170000}'), (
+        '1', '()', '{"name":"Apple Computer Company","type":["company","start-up"],"founded":"April 1, 1976","id":1}'), ('4', '->', '{"action":"divested","amount":800,"date":"April 12, 1976"}'), ('4', '()', '{"name":"Ronald Wayne","type":["person","administrator","founder"],"id":4}'), ('1', '->', '{"action":"founded"}')])
+    assert _normalize_results(db.traverse_with_bodies(database_test_file, 5, neighbors_fn=db.find_neighbors)) == _normalize_results([('5', '()', '{"name":"Mike Markkula","type":["person","investor"],"id":5}'), ('1', '->', '{"action":"invested","equity":80000,"debt":170000}'), ('1', '()', '{"name":"Apple Computer Company","type":["company","start-up"],"founded":"April 1, 1976","id":1}'), ('2', '<-', '{"action":"founded"}'), ('3', '<-', '{"action":"founded"}'), ('4', '<-', '{"action":"founded"}'), (
+        '5', '<-', '{"action":"invested","equity":80000,"debt":170000}'), ('4', '->', '{"action":"divested","amount":800,"date":"April 12, 1976"}'), ('2', '()', '{"name":"Steve Wozniak","type":["person","engineer","founder"],"id":2}'), ('1', '->', '{"action":"founded"}'), ('3', '->', '{}'), ('3', '()', '{"name":"Steve Jobs","type":["person","designer","founder"],"id":3}'), ('2', '<-', '{}'), ('4', '()', '{"name":"Ronald Wayne","type":["person","administrator","founder"],"id":4}'), ('1', '<-', '{"action":"divested","amount":800,"date":"April 12, 1976"}')])
+
+
 def test_visualization(database_test_file, apple, tmp_path):
     dot_raw = tmp_path / "apple-raw.dot"
     db.visualize(database_test_file, dot_raw, [4, 1, 5])
@@ -130,4 +144,16 @@ def test_visualization(database_test_file, apple, tmp_path):
     dot = tmp_path / "apple.dot"
     db.visualize(database_test_file, dot, [4, 1, 5], exclude_node_keys=[
                  'type'], hide_edge_key=True)
+    assert cmp(dot, Path.cwd() / ".." / ".examples" / "apple.dot")
+
+
+def test_visualize_bodies(database_test_file, apple, tmp_path):
+    dot_raw = tmp_path / "apple-raw.dot"
+    path_with_bodies = db.traverse_with_bodies(database_test_file, 4, 5)
+    db.visualize_bodies(dot_raw, path_with_bodies)
+    here = Path(__file__).parent.resolve()
+    assert cmp(dot_raw, here / "fixtures" / "apple-raw.dot")
+    dot = tmp_path / "apple.dot"
+    db.visualize_bodies(dot, path_with_bodies, exclude_node_keys=[
+                        'type'], hide_edge_key=True)
     assert cmp(dot, here / "fixtures" / "apple.dot")
