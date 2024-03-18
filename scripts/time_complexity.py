43295b51483322c7dd3fd29ea4e6a6b3301f1e73
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-03-18 11:39:27 +0530

    added script

---
+++ scripts/time_complexity.py
@@ -0,0 +1,86 @@
+import sys
+import os
+import time
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
+sys.path.append(parent_dir)
+
+from libsql_graph_db import database as db
+
+
+def measure_execution_time(operation_func, *args, **kwargs):
+    start_time = time.time()
+    res = operation_func(*args, **kwargs)
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return res, time_taken
+
+
+def time_complexity():
+    """
+    Main Function to calculate the time duration of each operation.
+    """
+
+    operations = [
+        (db.find_node, "Student"),
+        (db.find_nodes, [db._generate_clause("subject", predicate="LIKE")], ("Civil%")),
+        (db.add_node, {"subject": "CSE", "type": ["person", "PHD"]}, "Professor"),
+        (
+            db.add_nodes,
+            [
+                {"name": "Stanley", "age": "20"},
+                {"name": "Jenny", "age": "55"},
+            ],
+            ["Student", "Principal"],
+        ),
+        (db.upsert_node, {"name": "Bob", "age": "23"}, "Student"),
+        (
+            db.upsert_nodes,
+            [
+                {"name": "Stanley", "age": "30"},
+                {"name": "James", "age": "35"},
+            ],
+            ["Student", "Professor"],
+        ),
+        (db.connect_nodes, "Professor", "Student", {"teaches": "CS"}),
+        (
+            db.connect_many_nodes,
+            ["Professor", "Student"],
+            ["Student", "Principal"],
+            [{"teaches": "CS"}, {"type": "intelligent"}],
+        ),
+        (db.remove_node, "Professor"),
+        (db.remove_nodes, "Engineer"),
+        (
+            db.traverse,
+            os.getenv("LIBSQL_URL"),
+            os.getenv("LIBSQL_AUTH_TOKEN"),
+            "Student",
+        ),
+    ]
+
+    results_dict = {}
+
+    for operation, *args in operations:
+        try:
+            result, duration = measure_execution_time(operation, *args)
+            operation_name = operation.__name__
+            results_dict[operation_name] = duration
+        except Exception as err:
+            logging.error(f"Exception: {err}")
+
+    logging.info(results_dict)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        filename="scripts/operations.log",
+        level=logging.DEBUG,
+    )
+
+    time_complexity()
---
+++ tests/visualizers_test.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from filecmp import cmp
+from libsql_graph_db import database as db
+from libsql_graph_db import visualizers as viz
+from test_common import database_test_file, nodes, edges, apple
+
+
+def test_visualization(database_test_file, apple, tmp_path):
+    dot_raw = tmp_path / "apple-raw.dot"
+    viz.graphviz_visualize(database_test_file, dot_raw, [4, 1, 5])
+    here = Path(__file__).parent.resolve()
+    assert cmp(dot_raw, here / "fixtures" / "apple-raw.dot")
+    dot = tmp_path / "apple.dot"
+    viz.graphviz_visualize(
+        database_test_file,
+        dot,
+        [4, 1, 5],
+        exclude_node_keys=["type"],
+        hide_edge_key=True,
+    )
+    assert cmp(dot, here / "fixtures" / "apple.dot")
+
+
+def test_visualize_bodies(database_test_file, apple, tmp_path):
+    dot_raw = tmp_path / "apple-raw.dot"
+    path_with_bodies = db.traverse(database_test_file, 4, 5, with_bodies=True)
+    viz.graphviz_visualize_bodies(dot_raw, path_with_bodies)
+    here = Path(__file__).parent.resolve()
+    assert cmp(dot_raw, here / "fixtures" / "apple-raw.dot")
+    dot = tmp_path / "apple.dot"
+    viz.graphviz_visualize_bodies(
+        dot, path_with_bodies, exclude_node_keys=["type"], hide_edge_key=True
+    )
+    assert cmp(dot, here / "fixtures" / "apple.dot")
