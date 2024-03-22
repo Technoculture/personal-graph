24ef35938c1574e7e3095088509343921bb13a09
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-03-22 10:43:25 +0530

    updated dimensions and model

--- libsql_graph_db/database.py
+++ libsql_graph_db/database.py
@@ -23,9 +23,15 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
 
 
-def get_embedding(text, model="text-embedding-ada-002"):
+def get_embedding(text, model="text-embedding-3-small"):
     text = text.replace("\n", " ")
-    return client.embeddings.create(input=[text], model=model).data[0].embedding
+    return (
+        client.embeddings.create(
+            input=[text], model=model, dimensions=384, encoding_format="float"
+        )
+        .data[0]
+        .embedding
+    )
 
 
 @lru_cache(maxsize=None)
