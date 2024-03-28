e81dce7490943f4dfc7493456afaf23750d7303f
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-03-28 12:48:52 +0530

    added models

--- libsql_graph_db/models.py
+++ libsql_graph_db/models.py
@@ -1,3 +1,24 @@
 """
 Pydantic models for pythonic API to the graph db
 """
+
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class Node(BaseModel):
+    id: int
+    label: str
+    color: str
+
+
+class Edge(BaseModel):
+    source: int
+    target: int
+    label: str
+    color: str = "black"
+
+
+class KnowledgeGraph(BaseModel):
+    nodes: List[Node] = Field(..., default_factory=list)
+    edges: List[Edge] = Field(..., default_factory=list)
