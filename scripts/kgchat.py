2cc643a31af735e40f5a34ada6834102d039424c
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-04-13 16:25:02 +0530

    streamlit app: added chat elements

--- scripts/kgchat.py
+++ scripts/kgchat.py
@@ -1,42 +1,77 @@
+import json
+
 import streamlit as st
-import sys
 import os
+import dspy  # type: ignore
+import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from personal_graph.graph import Graph
-import os
-from examples.dspy_program import RAG
 
-rag = RAG(depth=2)
+from personal_graph.graph import Graph
+from personal_graph.retriever import PersonalRM
+
+turbo = dspy.OpenAI(model="gpt-3.5-turbo", api_key=os.getenv("OPEN_API_KEY"))
+retriever = PersonalRM(
+    db_url=os.getenv("LIBSQL_URL"), auth_token=os.getenv("LIBSQL_AUTH_TOKEN"), k=2
+)
+
+dspy.settings.configure(lm=turbo, rm=retriever)
+
+
+class GenerateAnswer(dspy.Signature):
+    """Answer questions with short factoid answers."""
+
+    context = dspy.InputField(desc="may contain relevant facts from user's graph")
+    question = dspy.InputField()
+    answer = dspy.OutputField(
+        desc="a short answer to the question, deduced from the information found in the user's graph"
+    )
+
+
+class RAG(dspy.Module):
+    def __init__(self, depth=3):
+        super().__init__()
+        self.retrieve = dspy.Retrieve(k=depth)
+        self.generate_answer = dspy.ChainOfThought(GenerateAnswer)
+
+    def forward(self, question):
+        context = self.retrieve(question).passages
+        prediction = self.generate_answer(context=context, question=question)
+        return dspy.Prediction(context=context, answer=prediction.answer)
 
 
 def main():
+    rag = RAG(depth=2)
     st.title("Knowledge Graph Chat")
 
     graph = Graph(os.getenv("LIBSQL_URL"), os.getenv("LIBSQL_AUTH_TOKEN"))
-    chat_history = []
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    user_backstory = st.sidebar.chat_input("Enter your backstory")
+    if st.sidebar.button("Load Backstory"):
+        graph.insert(user_backstory)
+        st.sidebar.success("Backstory loaded successfully!")
 
     st.sidebar.title("Graph Visualization")
-    graph_plot = st.sidebar.empty()
 
-    user_input = st.text_input("You: ", key="user_input")
-    if user_input:
-        chat_history.append({"role": "user", "content": user_input})
-        graph.insert(user_input)
-        kg = graph.search_query(user_input)
+    if prompt := st.chat_input("Say Something?"):
+        with st.chat_message("user"):
+            response = rag(prompt)
 
-        if kg.nodes:
-            graph_plot.graphviz_chart(graph.visualize_graph(kg))
-        else:
-            graph_plot.warning("No nodes found for the query.")
-
-        response = rag(user_input)
-        chat_history.append({"role": "assistant", "content": response.answer})
-        for chat in chat_history:
-            if chat["role"] == "user":
-                st.write("You: " + chat["content"])
+            if isinstance(response.context, str):
+                st.markdown(response.context)
             else:
-                st.write("Assistant: " + chat["content"])
+                for content in response.context:
+                    content_dict = json.loads(content)
+                    st.markdown(content_dict["body"])
 
 
 if __name__ == "__main__":
