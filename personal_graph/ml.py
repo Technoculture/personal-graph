224428f5a8543df31cd8cc53f6d0fc4caa6db6e8
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-04-22 16:18:27 +0530

    feature: added to_networkX method

--- personal_graph/ml.py
+++ personal_graph/ml.py
@@ -1,4 +1,37 @@
-"""
-Provide functions to convert the data in the graph db into formats compatible with
-graph analysis frameworks such as Networkx and DGL
-"""
+import json
+import networkx as nx  # type: ignore
+import matplotlib.pyplot as plt
+from personal_graph.graph import Graph
+
+
+def to_networkx(graph: Graph) -> nx.DiGraph:
+    """
+    Convert the graph database to a NetworkX DiGraph object.
+    """
+    G = nx.DiGraph()  # Empty Graph with no nodes and edges
+
+    # Add nodes
+    node_ids = graph.fetch_ids_from_db()
+    for node_id in node_ids:
+        node_data = graph.search_node(node_id)
+        G.add_node(node_id, **node_data)
+
+    # Add edges
+    for source_id in node_ids:
+        for target_id, _, edge_data in graph.traverse(source_id, with_bodies=True):
+            G.add_edge(source_id, target_id, **(json.loads(edge_data)))
+
+    # Visualizing the NetworkX Graph
+    plt.figure(figsize=(10, 8))
+    nx.draw(
+        G,
+        with_labels=True,
+        node_color="skyblue",
+        edge_color="gray",
+        font_size=8,
+        node_size=500,
+    )
+    plt.axis("off")
+    plt.show()
+
+    return G
