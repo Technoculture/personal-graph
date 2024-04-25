ba909cc0f631931a7db54cc2adb070e1a0073d99
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-04-25 11:28:24 +0530

    bugfix: test_ml.py

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,3 +1,4 @@
+import json
 import networkx as nx  # type: ignore
 from personal_graph.ml import to_networkx, from_networkx
 import personal_graph.graph as pg
@@ -14,9 +15,11 @@ def test_from_networkx(mock_personal_graph, mock_atomic, mock_db_connection_and_
     networkx_graph = nx.Graph()
     networkx_graph.add_node("1", label="Sample Label", attribute={"Person": "scholar"})
     networkx_graph.add_node(
-        "2", label="Researching", attribute={"University": "Stanford"}
+        "2", label="Researching", attribute=json.dumps({"University": "Stanford"})
     )
-    networkx_graph.add_edge("1", "2", label="has", attribute={"Person": "University"})
+    networkx_graph.add_edge(
+        "1", "2", label="has", attribute=json.dumps({"Person": "University"})
+    )
 
     personal_graph = from_networkx(networkx_graph)
 
