28fa987913629d05aaaa2e720599d3aa523d73db
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-04-29 16:32:46 +0530

    refactor: use high level apis

--- examples/nl_interface.py
+++ examples/nl_interface.py
@@ -4,65 +4,62 @@ import openai
 import logging
 import argparse
 from dotenv import load_dotenv
-from personal_graph import database as db
-from personal_graph import natural
+from personal_graph import Graph, natural
 from personal_graph.embeddings import OpenAIEmbeddingsModel
 
 
 def main(args):
-    # Initialize the Database
-    db.initialize(args.db_url, args.db_auth_token)
-
-    # LLM client
-    llm_client = openai.OpenAI(
-        api_key="",
-        base_url=args.llm_base_url,
-        default_headers={"Authorization": f"Bearer {args.llm_token}"},
-    )
-
-    # Embedding client
-    embedding_client = (
-        openai.OpenAI(
+    with Graph(db_url=args.db_url, db_auth_token=args.db_auth_token) as graph:
+        # LLM client
+        llm_client = openai.OpenAI(
             api_key="",
-            base_url=args.embeddings_base_url,
-            default_headers={"Authorization": f"Bearer {args.embeddings_token}"},
-        )
-        if args.embeddings_base_url and args.embeddings_token
-        else None
-    )
-
-    embedding_model = OpenAIEmbeddingsModel(
-        embedding_client, args.embeddings_model_name, args.embedding_model_dimension
-    )
-
-    # Testing insert query into graph db
-    nl_query = "increased thirst, weight loss, increased hunger, frequent urination etc. are all symptoms of diabetes."
-    graph = natural.insert_into_graph(
-        text=nl_query,
-        llm_client=llm_client,
-        llm_model_name=args.llm_model_name,
-        embedding_model=embedding_model,
-    )
-
-    logging.info("Nodes in the Knowledge Graph: \n")
-    for node in graph.nodes:
-        logging.info(
-            f"ID: {node.id}, Label: {node.label}, Attribute: {node.attributes}"
+            base_url=args.llm_base_url,
+            default_headers={"Authorization": f"Bearer {args.llm_token}"},
         )
 
-    logging.info("Edges in the Knowledge Graph: \n")
-    for edge in graph.edges:
-        logging.info(
-            f"Source: {edge.source}, Target: {edge.target}, Label: {edge.label}, Attribute: {edge.attributes}"
+        # Embedding client
+        embedding_client = (
+            openai.OpenAI(
+                api_key="",
+                base_url=args.embeddings_base_url,
+                default_headers={"Authorization": f"Bearer {args.embeddings_token}"},
+            )
+            if args.embeddings_base_url and args.embeddings_token
+            else None
         )
 
-    # Testing search query from graph db
-    search_query = "I am losing my weight too frequent."
-    knowledge_graph = natural.search_from_graph(
-        search_query, embedding_model=embedding_model
-    )
+        embedding_model = OpenAIEmbeddingsModel(
+            embedding_client, args.embeddings_model_name, args.embedding_model_dimension
+        )
 
-    logging.info(f"Knowledge Graph: \n{knowledge_graph}")
+        # Testing insert query into graph db
+        nl_query = "increased thirst, weight loss, increased hunger, frequent urination etc. are all symptoms of diabetes."
+        graph = natural.insert_into_graph(
+            text=nl_query,
+            llm_client=llm_client,
+            llm_model_name=args.llm_model_name,
+            embedding_model=embedding_model,
+        )
+
+        logging.info("Nodes in the Knowledge Graph: \n")
+        for node in graph.nodes:
+            logging.info(
+                f"ID: {node.id}, Label: {node.label}, Attribute: {node.attributes}"
+            )
+
+        logging.info("Edges in the Knowledge Graph: \n")
+        for edge in graph.edges:
+            logging.info(
+                f"Source: {edge.source}, Target: {edge.target}, Label: {edge.label}, Attribute: {edge.attributes}"
+            )
+
+        # Testing search query from graph db
+        search_query = "I am losing my weight too frequent."
+        knowledge_graph = natural.search_from_graph(
+            search_query, embedding_model=embedding_model
+        )
+
+        logging.info(f"Knowledge Graph: \n{knowledge_graph}")
 
 
 if __name__ == "__main__":
