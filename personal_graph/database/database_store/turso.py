ffaaac1aae8a24dd4726aa9c70db8557e8e64945
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-05-21 15:05:22 +0530

    refactor: updated tursodb class

--- personal_graph/database/database_store/turso.py
+++ personal_graph/database/database_store/turso.py
@@ -1,16 +1,50 @@
+from functools import lru_cache
+from pathlib import Path
+
 import libsql_experimental as libsql  # type: ignore
-from typing import Optional, Any, Callable
+from typing import Optional, Any, Callable, Tuple
+
+from jinja2 import BaseLoader, Environment, select_autoescape
 
 from personal_graph.database.database_store.sqlite import SQLite
 
 CursorExecFunction = Callable[[libsql.Cursor, libsql.Connection], Any]
 
 
+@lru_cache(maxsize=None)
+def read_sql(sql_file: Path) -> str:
+    with open(Path(__file__).parent.resolve() / "sql" / sql_file) as f:
+        return f.read()
+
+
+class SqlTemplateLoader(BaseLoader):
+    def __init__(self, templates_dir: Path):
+        self.templates_dir = templates_dir
+
+    def get_source(
+        self, environment: Environment, template: str
+    ) -> Tuple[str, str, Callable[[], bool]]:
+        def uptodate() -> bool:
+            return True
+
+        template_path = self.templates_dir / template
+
+        # Return the source code, the template name, and the uptodate function
+        return read_sql(template_path), template, uptodate
+
+
 class TursoDB(SQLite):
     def __init__(self, *, url: Optional[str] = None, auth_token: Optional[str] = None):
-        super().__init__()
         self.db_url = url
         self.db_auth_token = auth_token
+
+        self.env = Environment(
+            loader=SqlTemplateLoader(Path(__file__).parent / "sql"),
+            autoescape=select_autoescape(),
+        )
+        self.clause_template = self.env.get_template("search-where.template")
+        self.search_template = self.env.get_template("search-node.template")
+        self.traverse_template = self.env.get_template("traverse.template")
 
     def __eq__(self, other):
         return self.db_url == other.db_url
