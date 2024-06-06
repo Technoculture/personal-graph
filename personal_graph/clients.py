5e28c8a6bf0bbd0b93a4ce8c5ca0090021ef516a
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-06-06 13:15:53 +0530

    refactor: updated OpenAIClient

--- personal_graph/clients.py
+++ personal_graph/clients.py
@@ -110,7 +110,7 @@ class OpenAIClient(APIClient):
         self.client = self._create_default_client(*args, **kwargs)
 
     def _create_default_client(self, *args, **kwargs):
-        return openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY", ""), *args, **kwargs)
+        return openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY", self.api_key), *args, **kwargs)
 
 
 @dataclass
