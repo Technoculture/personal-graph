43c0ad05074004431c3161890233c92897b9e958
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-06-11 15:50:51 +0530

    refactor: added exceptions to from_rdf method

--- personal_graph/ontology.py
+++ personal_graph/ontology.py
@@ -1,15 +1,27 @@
 import logging
 import owlready2  # type: ignore
+from owlready2 import OwlReadyOntologyParsingError
 
 
 def from_rdf(file_path: str) -> owlready2.namespace.Ontology:
     """
-    This method will load an ontology.
-    @param file_path: path to ontology file
-    @return: Ontology
+    Load an ontology from a specified file path.
+
+    @param file_path: Path to the ontology file
+
+    @return: owlready2.namespace.Ontology: The loaded ontology
+
+    @raise: OwlReadyOntologyParsingError: Unable to parse the specified file
     """
+    try:
+        onto = owlready2.get_ontology(file_path).load()  # Loading the rdf file
+        logging.info(onto)
+        return onto
 
-    onto = owlready2.get_ontology(file_path).load()  # Loading the rdf file
+    except OwlReadyOntologyParsingError as err:
+        raise OwlReadyOntologyParsingError(
+            f"Error loading ontology: {str(err)}"
+        ) from err
 
-    logging.info(onto)
-    return onto
+    except FileNotFoundError as ferr:
+        raise FileNotFoundError(f"File not found: {str(ferr)}") from ferr
