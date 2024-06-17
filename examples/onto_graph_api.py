ae64f98b57a53173a295f245bbcde3884e79bea4
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-06-17 18:17:42 +0530

    refactor: updated onto_graph examples

--- examples/onto_graph_api.py
+++ examples/onto_graph_api.py
@@ -4,10 +4,19 @@ from personal_graph.ontology import from_rdf
 
 ontologies = from_rdf("/path/to/rdffile")
 
-with OntoGraph(ontologies=[ontologies]) as graph_db:
+with OntoGraph(ontologies=[ontologies]) as db:
     try:
-        node = Node(id="1", label="Pabelo", attributes={"age": 30})
-        graph_db.add_node(node, "device")
-        graph_db.visualize("sample.dot", ["1"])
+        node = Node(
+            id="1",
+            label="Pabelo",
+            attributes={
+                "entity": "sample entity",
+                "group": "management",
+                "medication": "instant",
+                "individual": "personality",
+            },
+        )
+        db.add_node(node, node_type="administrative")
+        db.visualize("sample.dot", ["1"])
     except ValueError as e:
         print(f"Error adding node: {e}")
