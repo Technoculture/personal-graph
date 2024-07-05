408db3acbc62dea2e77dc911adb0a5b45f133cef
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-07-05 17:35:18 +0530

    feature: script to dump classes into .pkl

---
+++ personal_graph/extract_classes.py
@@ -0,0 +1,21 @@
+"""
+This script creates a .pkl file of all the classes along with their properties that fhir ontology has.
+"""
+import logging
+import pickle
+from personal_graph.helper import extract_classes_properties
+
+# Dumping the fhir ontology's classes and properties
+node_types_info = extract_classes_properties()
+file = open("ontology_classes_and_properties.pkl", "wb")
+pickled_data = pickle.dumps(node_types_info)
+file.write(pickled_data)
+file.close()
+
+
+# Loading up the classes and properties of fhir ontology
+with open("ontology_classes_and_properties.pkl", "rb") as pickle_file:
+    loaded_data = pickle.load(pickle_file)
+
+# loaded data (loaded_data)
+logging.info(loaded_data)
