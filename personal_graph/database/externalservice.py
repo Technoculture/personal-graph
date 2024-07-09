c7fef5e16671d0a1f394ab4098840d20c9a2cc10
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-07-09 12:32:18 +0530

    feature: added abc for ExternalService

---
+++ personal_graph/database/externalservice.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Optional, Any
+
+from personal_graph.database.db import DB
+
+
+class ExternalService(DB, ABC):
+    """
+    Abstract Base Class for implementing external service connections.
+    This class inherits from DB and adds methods specific to external service connections.
+    """
+
+    @abstractmethod
+    def connect(self, url: str):
+        """Establish a connection to the external service"""
+        pass
+
+    @abstractmethod
+    def disconnect(self):
+        """Close the connection to the external service"""
+        pass
+
+    @abstractmethod
+    def execute_request(
+        self, method: str, endpoint: str, data: Optional[Dict] = None
+    ) -> Any:
+        """Execute a REST API request to the external service"""
+        pass
