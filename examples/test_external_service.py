f00221e0e4c6ab117d3b82768e1fb90a52383c21
anubhuti24 <anubhutibhardwaj11@gmail.com>
2024-07-11 12:59:22 +0530

    refactor: updated example

--- examples/test_external_service.py
+++ examples/test_external_service.py
@@ -1,11 +1,9 @@
 from personal_graph import Node
 from personal_graph.database.fhirservice.fhirservice import FhirService
 
-fhir_ontologies = ["Patient", "Observation", "Encounter", "Practitioner"]
-fhir_service = FhirService(ontologies=fhir_ontologies)
+fhir_service = FhirService(url="http://127.0.0.1:9008/resource")
 
 # Connect to the FHIR server
-fhir_service.connect("http://localhost/resource")
 db_url = "libsql://db-919113317500-technoculture.turso.io"
 
 # Create a patient
@@ -16,11 +14,11 @@ patient_data = {
     "gender": "male",
     "birthDate": "1970-01-01",
 }
-patient_id = fhir_service.add_node(db_url, "Patient", patient_data)
+patient_id = fhir_service.add_node("Patient", patient_data, db_url)
 print(f"Created patient with ID: {patient_id}")
 
 # Search patient
-patient = fhir_service.search_node(db_url, "2290", "Patient")
+patient = fhir_service.search_node("2290", db_url, "Patient")
 print(patient)
 
 # Update patient data
@@ -32,9 +30,9 @@ updated_patient_data = {
     "birthDate": "1970-01-01",
 }
 node = Node(id="2290", label="Patient", attributes=updated_patient_data)
-fhir_service.update_node(db_url, node)
+fhir_service.update_node(node, db_url)
 
 # Remove a patient
-fhir_service.remove_node(db_url, "2290", "Patient")
+fhir_service.remove_node("2290", db_url, "Patient")
 
 fhir_service.disconnect()
